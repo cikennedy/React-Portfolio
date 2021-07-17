1e1a4ffa7c970e0302240882257e92b9d674ec97
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-16 20:56:29 -0500

    added template profile page from class example

---
+++ src/pages/Profile.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Project from "../components/Project";
+import Jumbotron from "../components/Jumbotron";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+
+
+function Profile() {
+  // Setting our component's initial state
+  const [books, setBooks] = useState([])
+ 
+
+
+
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>What Books Should I Read?</h1>
+            </Jumbotron>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Books On My List</h1>
+            </Jumbotron>
+            {/* {books.length ? (
+              <List>
+                {books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )} */}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
+
+export default Profile;
